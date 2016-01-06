9b2e67eb7642c8323661b99aa0ba1c9955a0960f
shunyi <M157q.tw@gmail.com>
2016-01-06 16:28:43 +0800

    Add web part built up by Node.js

--- web/app.js
+++ web/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var upload = require('./routes/upload');
@@ -58,7 +57,6 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 
 module.exports = app;
